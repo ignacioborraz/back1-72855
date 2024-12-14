bb063929bb223cc7856faa2ac054c9da1aed97ba
ignacioborraz <ignacioborraz@hotmail.com>
2024-12-14 13:10:08 -0300

    variables

---
+++ 01_back/personajes.js
@@ -0,0 +1,34 @@
+const nombre1 = "Peter Parker";
+const ciudad1 = "New York";
+let edad1 = 18;
+const habilidades1 = [
+  "Superhuman strength, agility, reflexes, and durability",
+  "Ability to cling to solid surfaces",
+  "Precognitive spider-sense",
+  "Genius-level intellect",
+  "Skilled hand-to-hand combatant",
+  "Proficient scientist and engineer",
+  "Utilizes wrist-mounted web-shooters",
+];
+edad1 = edad1 + 5;
+
+console.log(nombre1);
+console.log(ciudad1);
+console.log(edad1);
+console.log(habilidades1);
+
+const nombre2 = "Natasha Romanova";
+const ciudad2 = "New York";
+let edad2 = 30;
+const habilidades2 = [
+  "Expert marksman and mastery of various weapons",
+  "Expert martial artist and hand-to-hand combatant",
+  "Slowed aging, and enhanced immune system",
+  "Expert spy",
+];
+edad2 += 5;
+
+console.log(nombre2);
+console.log(ciudad2);
+console.log(edad2);
+console.log(habilidades2);
