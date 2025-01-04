4d286e218ca1b5acdbb6df9e56b7b5d3bdd04607
ignacioborraz <ignacioborraz@hotmail.com>
2025-01-04 13:22:17 -0300

    router

--- 03_npm_fs/index.js
+++ 03_npm_fs/index.js
@@ -1,6 +1,7 @@
 import http from "http";
+import router from "./router.js";
 
-const server = http.createServer();
+const server = http.createServer(router);
 const port = 8080;
 const ready = () => console.log("server ready on port " + port);
 server.listen(port, ready);
