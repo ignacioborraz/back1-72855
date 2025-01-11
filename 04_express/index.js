898b355947813263451d15c83852a2299b8e5996
ignacioborraz <ignacioborraz@hotmail.com>
2025-01-11 13:08:08 -0300

    queries & params

--- 04_express/index.js
+++ 04_express/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import productsManager from "./src/data/fs/products.fs.js";
 
 /* server settings */
 const server = express();
@@ -18,18 +19,39 @@ const apiPoint = "/api";
 const apiCb = (req, res) => res.status(200).send("WELCOME TO MY API");
 server.get(apiPoint, apiCb);
 
-server.get("/api/:name/:age", (req, res) => {
+const readOneProduct = async (req, res) => {
+  const { pid } = req.params;
+  const one = await productsManager.readOne(pid);
+  if (one) {
+    return res.status(200).json({ response: one });
+  }
+  return res.status(404).json({ response: "Not found!" });
+};
+server.get("/api/products/:pid", readOneProduct);
+
+const readProducts = async (req, res) => {
+  const { category } = req.query;
+  const all = await productsManager.readAll(category);
+  if (all.length > 0) {
+    return res.status(200).json({ response: all });
+  }
+  return res.status(404).json({ response: "Not found!" });
+};
+server.get("/api/products", readProducts);
+
+/* server.get("/api/:name/:age", (req, res) => {
   //console.log(req.params);
   const { name, age } = req.params;
   const message = `Hola ${name}! Tienes ${age} años!`;
   return res.status(200).send(message);
-});
+}); */
 
-server.get("/api/welcome", (req, res) => {
+/* server.get("/api/welcome", (req, res) => {
   //console.log(req.query);
   const { name, age } = req.query;
   const message = `Hola ${name || "Coder"}! Tienes ${age || 18} años!`;
   return res.status(200).send(message);
-});
+}); */
+
 /* las rutas inexistes SIEMPRE van al final*/
 server.get("*", (req, res) => res.status(404).send("NOT FOUND POINT"));
