53c960798a14d6149b9f5ab87131661cc221a64c
ignacioborraz <ignacioborraz@hotmail.com>
2025-01-18 15:29:20 -0300

    hbs

--- 05_router_hbs/index.js
+++ 05_router_hbs/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import { engine } from "express-handlebars";
+import __dirname from "./utils.js";
 import router from "./src/routers/index.router.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
@@ -9,6 +11,11 @@ const server = express();
 const port = 8080;
 const ready = () => console.log("server ready on port " + port);
 server.listen(port, ready);
+
+/* template engine */
+server.engine("handlebars", engine());
+server.set("view engine", "handlebars");
+server.set("views", __dirname + "/src/views");
 
 /* middlewares */
 server.use(morgan("dev"));
